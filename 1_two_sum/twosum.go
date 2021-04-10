74dcf563a3a4d8534588e09ab704ba4132d68b55
Pavel Derendyaev <dddpaul@gmail.com>
2021-04-10 15:42:11 +0300

    Add solution

--- 1_two_sum/twosum.go
+++ 1_two_sum/twosum.go
@@ -1,5 +1,14 @@
 package main
 
 func TwoSum(nums []int, target int) []int {
-	return nil
+	for i, v1 := range nums {
+		for j, v2 := range nums {
+			if i != j {
+				if v1+v2 == target {
+					return []int{i, j}
+				}
+			}
+		}
+	}
+	return []int{}
 }
--- 1_two_sum/twosum_test.go
+++ 1_two_sum/twosum_test.go
@@ -13,3 +13,27 @@ func TestExample1(t *testing.T) {
 
 	assert.ElementsMatch(t, expected, TwoSum(nums, target))
 }
+
+func TestExample2(t *testing.T) {
+	nums := []int{3, 2, 4}
+	target := 6
+	expected := []int{1, 2}
+
+	assert.ElementsMatch(t, expected, TwoSum(nums, target))
+}
+
+func TestExample3(t *testing.T) {
+	nums := []int{3, 3}
+	target := 6
+	expected := []int{0, 1}
+
+	assert.ElementsMatch(t, expected, TwoSum(nums, target))
+}
+
+func TestNotFound(t *testing.T) {
+	nums := []int{3, 3}
+	target := 7
+	expected := []int{}
+
+	assert.ElementsMatch(t, expected, TwoSum(nums, target))
+}
