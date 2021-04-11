7372e39fd65fc809bf055ceae769145c93ddc9ac
Pavel Derendyaev <dddpaul@gmail.com>
2021-04-11 11:46:18 +0300

    Simplify code

--- 2_add_two_numbers/add_two_numbers.go
+++ 2_add_two_numbers/add_two_numbers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"fmt"
 )
 
 /**
@@ -15,7 +14,7 @@ type ListNode struct {
 }
 
 func New(nums []int) (*ListNode, error) {
-	var root, node, prev *ListNode
+	var head, node, prev *ListNode
 	for _, v := range nums {
 		if v < 0 || v > 9 {
 			return nil, errors.New("Non digit")
@@ -24,13 +23,13 @@ func New(nums []int) (*ListNode, error) {
 			Val: v,
 		}
 		if prev == nil {
-			root = node
+			head = node
 		} else {
 			prev.Next = node
 		}
 		prev = node
 	}
-	return root, nil
+	return head, nil
 }
 
 func (l *ListNode) Walk() []int {
@@ -46,21 +45,21 @@ func (l *ListNode) Walk() []int {
 	return arr
 }
 
-func Sum(v1 int, v2 int, overflow int) (int, int) {
-	fmt.Printf("%v, %v, %v\n", v1, v2, overflow)
-	sum := v1 + v2 + overflow
-	if sum >= 10 {
-		return sum - 10, 1
-	}
-	return sum, 0
-}
-
-func (l1 *ListNode) SumAll(l2 *ListNode) *ListNode {
-	var root, node, prev *ListNode
+func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var head, node, prev *ListNode
 	var sum, overflow, prev_overflow, v1, v2 int
 	var node1_walked, node2_walked bool
 	node1 := l1
 	node2 := l2
+
+	Sum := func(v1 int, v2 int, overflow int) (int, int) {
+		sum := v1 + v2 + overflow
+		if sum >= 10 {
+			return sum - 10, 1
+		}
+		return sum, 0
+	}
+
 	for {
 		if !node1_walked {
 			v1 = node1.Val
@@ -68,38 +67,36 @@ func (l1 *ListNode) SumAll(l2 *ListNode) *ListNode {
 		if !node2_walked {
 			v2 = node2.Val
 		}
+
 		sum, overflow = Sum(v1, v2, prev_overflow)
 		node = &ListNode{
 			Val: sum,
 		}
+
 		if prev == nil {
-			root = node
+			head = node
 		} else {
 			prev.Next = node
 		}
+
 		if node1.Next == nil {
 			node1_walked = true
 			v1 = 0
+		} else {
+			node1 = node1.Next
 		}
 		if node2.Next == nil {
 			node2_walked = true
 			v2 = 0
+		} else {
+			node2 = node2.Next
 		}
+
 		if node1_walked && node2_walked && overflow == 0 {
 			break
 		}
 		prev = node
 		prev_overflow = overflow
-		if !node1_walked {
-			node1 = node1.Next
-		}
-		if !node2_walked {
-			node2 = node2.Next
-		}
 	}
-	return root
-}
-
-func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return l1.SumAll(l2)
+	return head
 }
