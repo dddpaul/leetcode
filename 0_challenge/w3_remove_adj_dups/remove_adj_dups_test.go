094fa5ae0f2cd7921cacc9dd3fc5ddd28124b701
Pavel Derendyaev <dddpaul@gmail.com>
2021-04-18 23:17:47 +0300

    Add results

--- 0_challenge/w3_remove_adj_dups/remove_adj_dups_test.go
+++ 0_challenge/w3_remove_adj_dups/remove_adj_dups_test.go
@@ -86,7 +86,7 @@ func TestCreateWalkSeek(t *testing.T) {
 	assert.Equal(t, "cba", String(Seek(last, -1), false))
 }
 
-var letters = []rune("abcdefghijklmnopqrstuvwxyz")
+var letters = []rune("abc")
 
 func randSeq(n int) string {
 	b := make([]rune, n)
@@ -98,7 +98,7 @@ func randSeq(n int) string {
 
 func benchmark(s Solver, k int, b *testing.B) {
 	d := TestCase{
-		s:        randSeq(1_000_000),
+		s:        randSeq(1_000),
 		k:        k,
 		expected: "",
 	}
@@ -107,26 +107,39 @@ func benchmark(s Solver, k int, b *testing.B) {
 	}
 }
 
-func BenchmarkArray1(b *testing.B) {
-	benchmark(ArrayRewindSolver{}, 1, b)
+func BenchmarkArray2(b *testing.B) {
+	benchmark(ArrayRewindSolver{}, 2, b)
 }
 
-func BenchmarkArray10(b *testing.B) {
-	benchmark(ArrayRewindSolver{}, 10, b)
+func BenchmarkArray3(b *testing.B) {
+	benchmark(ArrayRewindSolver{}, 3, b)
 }
 
-func BenchmarkArray100(b *testing.B) {
-	benchmark(ArrayRewindSolver{}, 100, b)
+func BenchmarkArray4(b *testing.B) {
+	benchmark(ArrayRewindSolver{}, 4, b)
 }
 
-func BenchmarkList1(b *testing.B) {
-	benchmark(ListRewindSolver{}, 1, b)
+func BenchmarkList2(b *testing.B) {
+	benchmark(ListRewindSolver{}, 2, b)
 }
 
-func BenchmarkList10(b *testing.B) {
-	benchmark(ListRewindSolver{}, 10, b)
+func BenchmarkList3(b *testing.B) {
+	benchmark(ListRewindSolver{}, 3, b)
 }
 
-func BenchmarkList100(b *testing.B) {
-	benchmark(ListRewindSolver{}, 100, b)
+func BenchmarkList4(b *testing.B) {
+	benchmark(ListRewindSolver{}, 4, b)
 }
+
+/*
+goos: darwin
+goarch: amd64
+pkg: github.com/dddpaul/leetcode/0_challenge/w3_remove_adj_dups
+cpu: Intel(R) Core(TM) i9-9880H CPU @ 2.30GHz
+BenchmarkArray2-16    	   23611	     51462 ns/op
+BenchmarkArray3-16    	   84358	     18275 ns/op
+BenchmarkArray4-16    	  181688	      5942 ns/op
+BenchmarkList2-16     	   10000	    103323 ns/op
+BenchmarkList3-16     	    6933	    175089 ns/op
+BenchmarkList4-16     	    5958	    194401 ns/op
+*/
