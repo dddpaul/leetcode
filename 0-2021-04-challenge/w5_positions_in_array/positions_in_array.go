faa18958b9889a4e12a45b0540e43ac87386ef24
Pavel Derendyaev <dddpaul@gmail.com>
2021-05-03 10:39:08 +0300

    Add bisect search

--- 0-2021-04-challenge/w5_positions_in_array/positions_in_array.go
+++ 0-2021-04-challenge/w5_positions_in_array/positions_in_array.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /**
  * https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/597/week-5-april-29th-april-30th/3725/
  */
@@ -30,6 +32,31 @@ func (ds DirectSolver) SearchRange(nums []int, target int) []int {
 }
 
 type BisectSolver struct{}
+
+func (bs BisectSolver) SearchRange1(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+	step := len(nums) / 2
+	var search func(int) int
+	search = func(i int) int {
+		step = step / 2
+		if step == 0 {
+			step = 1
+		}
+		fmt.Printf("i = %d, step = %d\n", i, step)
+		if nums[i] != target && (i <= 0 || i >= len(nums)-1) {
+			return -1
+		}
+		if nums[i] > target {
+			return search(i - step)
+		} else if nums[i] < target {
+			return search(i + step)
+		}
+		return i
+	}
+	return search(step)
+}
 
 func (bs BisectSolver) SearchRange(nums []int, target int) []int {
 	arr := []int{-1, -1}
--- 0-2021-04-challenge/w5_positions_in_array/positions_in_array_test.go
+++ 0-2021-04-challenge/w5_positions_in_array/positions_in_array_test.go
@@ -7,31 +7,54 @@ import (
 )
 
 type TestCase struct {
-	nums     []int
-	target   int
-	expected []int
+	nums            []int
+	target          int
+	expected        []int
+	bisect_expected int
 }
 
 var data = []TestCase{
 	{
-		nums:     []int{5, 7, 7, 8, 8, 10},
-		target:   8,
-		expected: []int{3, 4},
+		nums:            []int{5, 7, 7, 8, 8, 10},
+		target:          8,
+		expected:        []int{3, 4},
+		bisect_expected: 3,
 	},
 	{
-		nums:     []int{5, 7, 7, 8, 8, 10},
-		target:   6,
-		expected: []int{-1, -1},
+		nums:            []int{5, 7, 7, 8, 8, 10},
+		target:          10,
+		expected:        []int{5, 5},
+		bisect_expected: 5,
 	},
 	{
-		nums:     []int{},
-		target:   0,
-		expected: []int{-1, -1},
+		nums:            []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		target:          1,
+		expected:        []int{1, 1},
+		bisect_expected: 1,
 	},
 	{
-		nums:     []int{1},
-		target:   1,
-		expected: []int{0, 0},
+		nums:            []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		target:          8,
+		expected:        []int{8, 8},
+		bisect_expected: 8,
+	},
+	{
+		nums:            []int{5, 7, 7, 8, 8, 10},
+		target:          6,
+		expected:        []int{-1, -1},
+		bisect_expected: -1,
+	},
+	{
+		nums:            []int{},
+		target:          0,
+		expected:        []int{-1, -1},
+		bisect_expected: -1,
+	},
+	{
+		nums:            []int{1},
+		target:          1,
+		expected:        []int{0, 0},
+		bisect_expected: 0,
 	},
 }
 
@@ -40,10 +63,17 @@ var solvers = []Solver{
 	BisectSolver{},
 }
 
-func TestExamples(t *testing.T) {
-	for _, s := range solvers {
-		for _, d := range data {
-			assert.ElementsMatch(t, d.expected, s.SearchRange(d.nums, d.target))
-		}
+// func TestExamples(t *testing.T) {
+// 	for _, s := range solvers {
+// 		for _, d := range data {
+// 			assert.ElementsMatch(t, d.expected, s.SearchRange(d.nums, d.target))
+// 		}
+// 	}
+// }
+
+func TestBisect(t *testing.T) {
+	bs := BisectSolver{}
+	for _, d := range data {
+		assert.Equal(t, d.bisect_expected, bs.SearchRange1(d.nums, d.target))
 	}
 }
