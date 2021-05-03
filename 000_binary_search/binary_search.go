6c616d6dd7ca2eb13d4fa409d97a4414930df10d
Pavel Derendyaev <dddpaul@gmail.com>
2021-05-03 21:11:22 +0300

    Add sol

--- 000_binary_search/binary_search.go
+++ 000_binary_search/binary_search.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 /**
  * https://leetcode.com/explore/learn/card/binary-search/138/background/1038/
  */
@@ -11,5 +16,29 @@ type Solver interface {
 type DirectSolver struct{}
 
 func (ds DirectSolver) Search(nums []int, target int) int {
-	return 0
+	if len(nums) == 0 || (nums[0] > target && nums[len(nums)-1] < target) {
+		return -1
+	}
+	step := len(nums) / 2
+	count := 0
+	limit := int(math.Ceil(math.Log2(float64(len(nums))))) + 1
+	var search func(int) int
+	search = func(i int) int {
+		step = step / 2
+		if step == 0 {
+			step = 1
+		}
+		fmt.Printf("count = %d, i = %d, step = %d, target = %d\n", count, i, step, target)
+		if (i < 0 || i > len(nums)-1) || count > limit {
+			return -1
+		}
+		count = count + 1
+		if nums[i] > target {
+			return search(i - step)
+		} else if nums[i] < target {
+			return search(i + step)
+		}
+		return i
+	}
+	return search(step)
 }
