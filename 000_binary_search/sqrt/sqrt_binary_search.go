41b25557a478c9b414d9369c9d74176c21d276db
Pavel Derendyaev <dddpaul@gmail.com>
2021-05-25 20:20:41 +0300

    Add sqrt binary search

--- 000_binary_search/sqrt/sqrt_binary_search.go
+++ 000_binary_search/sqrt/sqrt_binary_search.go
@@ -11,5 +11,21 @@ type Solver interface {
 type DirectSolver struct{}
 
 func (ds DirectSolver) MySqrt(x int) int {
-	return 0
+	if x == 0 || x == 1 {
+		return x
+	}
+	l, r := 0, x-1
+	for {
+		i := l + (r-l)/2
+		if i*i > x {
+			r = i - 1
+		} else if i*i < x {
+			l = i + 1
+		} else {
+			return i
+		}
+		if l > r {
+			return i
+		}
+	}
 }
