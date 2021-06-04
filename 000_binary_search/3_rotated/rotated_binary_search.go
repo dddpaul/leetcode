8d4ff739e22ba860f621341fbcd139449ff75ad1
Pavel Derendyaev <dddpaul@gmail.com>
2021-06-04 22:14:03 +0300

    Add find_pivot func

--- 000_binary_search/3_rotated/rotated_binary_search.go
+++ 000_binary_search/3_rotated/rotated_binary_search.go
@@ -10,6 +10,27 @@ type Solver interface {
 
 type DirectSolver struct{}
 
+func find_pivot(nums []int) int {
+	if len(nums) < 2 {
+		return -1
+	}
+	l, r := 0, len(nums)-1
+	for {
+		i := l + (r-l)/2
+		if nums[i] > nums[i+1] {
+			return i
+		}
+		if nums[0] < nums[i] {
+			r = i - 1
+		} else {
+			l = i + 1
+		}
+		if l > r {
+			return -1
+		}
+	}
+}
+
 func (ds DirectSolver) Search(nums []int, target int) int {
 	return 0
 }
--- 000_binary_search/3_rotated/rotated_binary_search_test.go
+++ 000_binary_search/3_rotated/rotated_binary_search_test.go
@@ -10,6 +10,7 @@ type TestCase struct {
 	nums     []int
 	target   int
 	expected int
+	pivot    int
 }
 
 var data = []TestCase{
@@ -17,26 +18,31 @@ var data = []TestCase{
 		nums:     []int{4, 5, 6, 7, 0, 1, 2},
 		target:   0,
 		expected: 4,
+		pivot:    3,
 	},
 	{
 		nums:     []int{4, 5, 6, 7, 0, 1, 3},
 		target:   3,
 		expected: 6,
+		pivot:    3,
 	},
 	{
 		nums:     []int{4, 5, 6, 7, 0, 1, 2},
 		target:   3,
 		expected: -1,
+		pivot:    3,
 	},
 	{
 		nums:     []int{1},
 		target:   0,
 		expected: -1,
+		pivot:    -1,
 	},
 	{
 		nums:     []int{},
 		target:   0,
 		expected: -1,
+		pivot:    -1,
 	},
 }
 
@@ -49,5 +55,11 @@ func TestExamples(t *testing.T) {
 		for _, d := range data {
 			assert.Equal(t, d.expected, s.Search(d.nums, d.target))
 		}
+	}
+}
+
+func TestFindPivot(t *testing.T) {
+	for _, d := range data {
+		assert.Equal(t, d.pivot, find_pivot(d.nums))
 	}
 }
