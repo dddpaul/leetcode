3fd7183ab6b495dc4c728ae65e55794ba3bd5f91
Pavel Derendyaev <dddpaul@gmail.com>
2021-06-07 21:10:29 +0300

    Working sol

--- 0-2021-05-challenge/w1_longest_sequence/longest_sequence.go
+++ 0-2021-05-challenge/w1_longest_sequence/longest_sequence.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 /**
  * https://leetcode.com/explore/featured/card/june-leetcoding-challenge-2021/603/week-1-june-1st-june-7th/3769/
  */
@@ -17,20 +13,24 @@ type DirectSolver struct{}
 func (ds DirectSolver) LongestConsecutive(nums []int) int {
 	m := make(map[int]int)
 	for _, i := range nums {
+		if _, ok := m[i]; ok {
+			continue
+		}
+
 		vPrev, okPrev := m[i-1]
 		vNext, okNext := m[i+1]
 		count := vPrev + vNext + 1
 
 		m[i] = count
 		if okPrev {
-			m[i-1] = count
+			m[i-1] = count     // update prev
+			m[i-vPrev] = count // update leftmost
 		}
 		if okNext {
-			m[i+1] = count
+			m[i+1] = count     // update next
+			m[i+vNext] = count // update rightmost
 		}
 	}
-
-	fmt.Printf("m = %v\n", m)
 
 	max := 0
 	for _, v := range m {
--- 0-2021-05-challenge/w1_longest_sequence/longest_sequence_test.go
+++ 0-2021-05-challenge/w1_longest_sequence/longest_sequence_test.go
@@ -21,6 +21,10 @@ var data = []TestCase{
 		nums:     []int{0, 3, 7, 2, 5, 8, 4, 6, 0, 1},
 		expected: 9,
 	},
+	{
+		nums:     []int{1, 2, 0, 1},
+		expected: 3,
+	},
 }
 
 var solvers = []Solver{
